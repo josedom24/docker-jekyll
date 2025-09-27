c62bd930628fe3fe058523002fd27243914da58c
José Domingo Muñoz <josedom24@gmail.com>
2025-09-27 20:15:44 +0200

    build

--- run.sh
+++ run.sh
@@ -10,12 +10,12 @@ REPO_DIR=$(realpath "$1")
 OUT_DIR=$(realpath "$2")
 
 # Construir imagen si no existe
-docker image inspect josedom24/pledin:1 >/dev/null 2>&1 || \
-  docker build -t josedom24/pledin:1 .
+docker image inspect docker-jekyll >/dev/null 2>&1 || \
+  docker build -t docker-jekyll .
 
 # Ejecutar el contenedor con rutas locales
 docker run --rm -it \
   -v "$REPO_DIR":"$REPO_DIR" \
   -v "$OUT_DIR":"$OUT_DIR" \
-  josedom24/pledin:1 \
+  docker-jekyll \
   /usr/local/bin/build-site.sh "$REPO_DIR" "$OUT_DIR"
